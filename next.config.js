3b6d97f6b3090d4c429fa3b2599552bfc2cc1e30
Norton29 <norton290194@gmail.com>
2021-10-18 21:09:30 -0300

    footer

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+// next.config.js
+const withImages = require('next-images')
+module.exports = withImages({
+ esModule: true
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable jsx-a11y/alt-text */
 import Head from 'next/head'
 import React, { Component } from 'react'
-import Form from './Form'
-import { Link } from 'react-router-dom'
+import Image from 'next/image'
+
 
 export default function Home() {
   return (
@@ -12,19 +13,19 @@ export default function Home() {
           <div className="main">
             <div className="center">
               <div className="menu">
-                <div className="logo">                  
+                <div className="logo">
                   <h1>FACILITA</h1>
                 </div>
 
                 <div className='pages h2'>
-                <h2>Quem Somos</h2> 
-                <h2><a href="form.tsx">Fale conosco</a></h2>
-                <h2>Tipos de empréstimos</h2>
+                  <h2>Quem Somos</h2>
+                  <h2><a href="form.tsx">Fale conosco</a></h2>
+                  <h2>Tipos de empréstimos</h2>
                 </div>
-                
+
                 <div className="item-menu">
                   <a href="#" > Login </a>
-                  </div>
+                </div>
 
               </div>
             </div>
@@ -32,13 +33,53 @@ export default function Home() {
         </div>
 
       </header>
-      <main>
-      <div>
-        
-      </div>
+
+      <body>
+
+        <div className="main_body">
+          <h1> HELLO WORLD</h1>
+          <h1> HELLO WORLD</h1>
+          <h1> HELLO WORLD</h1>
+          <h1> HELLO WORLD</h1>
+          <h1> HELLO WORLD</h1>
+          <h1> HELLO WORLD</h1>
 
 
-      </main>
-    </div>
+        </div>
+
+
+      </body>
+
+
+
+      <footer>
+        <div className="main_footer">
+          <div className="box">
+            <div className="navbar-nav">
+              <li><a href="">Sobre</a></li>
+              <li><a href="">Empregos</a></li>
+              <li><a href="">Novidades</a></li>
+              <li><a href="">Ajuda</a></li>
+              <h4>redes sociais</h4>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          </div>
+        </div>
+      </footer >
+    </div >
   )
 }
